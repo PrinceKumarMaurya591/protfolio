98fc25261ab8326a9a956d06e63b74cbf026bdc1
PrinceKumarMaurya591 <72147360+PrinceKumarMaurya591@users.noreply.github.com>
2023-11-27 18:57:14 +0530

    Add lucky-portfolio-master.zip

---
+++ My-personal-portfolio-master/My-personal-portfolio-master/src/components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Footer.css";
+import Wave from "../../img/wave.png";
+import Insta from "@iconscout/react-unicons/icons/uil-instagram";
+import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
+import Gitub from "@iconscout/react-unicons/icons/uil-github";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <img src={Wave} alt="" style={{ width: "100%" }} />
+      <div className="f-content">
+        <span>lucky292908@gmail.com</span>
+        <script>function Welcome() {
+
+          
+        }</script>
+        <div className="f-icons">
+        <a href="https://www.instagram.com/accounts/login/">
+          <Insta color="white" size={"3rem"} />
+          </a>
+          <a href="https://www.facebook.com/">
+          <Facebook color="white" size={"3rem"} />
+          </a>
+          
+          <a href="https://github.com/Lucky292908">
+          <Gitub color="white" size={"3rem"} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
